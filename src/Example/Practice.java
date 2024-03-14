1b3dd9f5d74b511ca8cdf32aa9c867061adba9c5
admin <admin@DESKTOP-1L2KML3>
2024-03-14 10:09:18 -1200

    githubcheck

---
+++ src/Example/Practice.java
@@ -0,0 +1,12 @@
+package Example;
+
+public class Practice {
+	public static void main(String args[]) {System.out.println("My first java prog");
+	System.out.println("java is very very easy");
+	System.out.println("java is very very difficult");
+
+	System.out.println("java is very very helpfull");
+
+	}
+
+}
